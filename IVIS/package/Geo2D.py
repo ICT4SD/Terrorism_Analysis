9f526f2e9538574e55c93fc0e164c239c1893756
Viola <xianzhi.cao@gmail.com>
2016-12-05 01:11:46 -0500

    Geo2D Map Updated

--- IVIS/package/Geo2D.py
+++ IVIS/package/Geo2D.py
@@ -2,24 +2,67 @@ from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-from package import feature
+from feature import *
+from heatmap import *
+from ipywidgets import *
 
 
-def plot_2D_density(dataset):
-    plt.figure(figsize=(18,15))
-    m = Basemap(projection='mill', llcrnrlon=-180, urcrnrlon=180,\
-                llcrnrlat=-70, urcrnrlat=85, resolution='c', )
-
+def plot_2D_density(Year, MapStyle):
+    df = user_df(Year)
+    plt.figure(figsize=(18,10))
+    
+    m = Basemap('mill')
     m.drawcountries(linewidth=0.5, linestyle='solid', color='w', antialiased=1, ax=None, zorder=None)
-    m.fillcontinents()
-    m.drawmapboundary()
 
-    lat = feature.make_list(dataset, 'latitude')
-    lon = feature.make_list(dataset, 'longitude')
+    if MapStyle == 'Blue Marble':
+        m.drawcoastlines()
+        m.bluemarble()
+    elif MapStyle == 'Etopo':
+        m.etopo()
+    else:
+        m.fillcontinents()
+        m.drawmapboundary()
+
+    lat = make_list(df, 'latitude')
+    lon = make_list(df, 'longitude')
 
     x,y = m(lon, lat)
-    m.plot(x, y, 'r.', marker='.', markersize=3, alpha=.5)
+    m.plot(x, y, 'r.', marker='o', markersize=3, alpha=.8)
 
-    plt.title('Global Attack Density: 1970-2015')
+    plt.title('Global Attack Density: {}-{}'.format(Year[0], Year[1]), size=16)
     plt.show()
+    
+    
+def user_df(Year):
+    '''
+    Input Parameter
+        - Year: Time Interval
+        
+    Return
+        - a DataFrame
+    '''
+    gt_df = IVIS().gt_df
+    df = gt_df[(gt_df.year <= Year[1]) & (gt_df.year >= Year[0])]
+    return df
+
+
+def year_interval():
+    yr_interval = IntRangeSlider(value=[1970, 2015],
+                                 min=1970,
+                                 max=2015,
+                                 step=1,
+                                 description='Year:',
+                                 disabled=False,
+                                 continuous_update=False,
+                                 orientation='horizontal',
+                                 readout=True,
+                                 readout_format='i',
+                                 slider_color='white',
+                                 color='black')
+    yr_interval.layout.width = '80%'
+    return yr_interval
+
+def map_styles():
+    return ('Blue Marble', 'Etopo', 'Plain')
+
     
