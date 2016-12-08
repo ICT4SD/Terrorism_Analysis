3d376959a17c191a98206cf7c189d40ee7bd2c4c
Caroline Roper <cer446@nyu.edu>
2016-12-08 12:37:50 -0500

    Updated dot plot to accept multiple inputs, added features to feature.py

--- IVIS/package/feature.py
+++ IVIS/package/feature.py
@@ -89,6 +89,34 @@ def make_five_year_start(dataset):
     dataset['period'] = [int(i/5)*5 for i in dataset.year]
     return dataset['period']
 
+### Caroline's functions
+
+def create_counts(data, group_by_columns, column_to_count):
+    '''Takes a list of column names and a column to count and counts rows by those column name, including nulls'''
+    all_columns = group_by_columns + [column_to_count]
+    data = data.loc[:,all_columns].fillna(value = 1)
+    grouped = data.groupby(group_by_columns)
+    return grouped.count()
+
+def unstack_table(data):
+    '''Unstacks data until there's only one row index remaining'''
+    data = data.fillna(0)
+    i = 0
+    while i < data.index.nlevels:
+        data = data.unstack()
+        i = i + 1
+    return data
+
+def remove_zeros(series):
+    return series[series > 0]
+
+def convert_series(series, label):
+    '''Takes a series and label for the series' values, returns df with 2 columns: the series' row index, & the series' values'''
+    converted = pd.DataFrame(series)
+    converted.columns = [label]
+    converted.reset_index(level=0, inplace=True)
+    return converted
+
 
 ### Caroline's make_five_year_start function goes here :)
 
